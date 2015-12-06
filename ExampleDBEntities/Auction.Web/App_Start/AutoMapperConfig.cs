9ae87ba43f0b49dd919031c7215999195035dfcd
Sanya Kaser <test.test@gmail.com>
2015-12-06 21:15:46 +0200

    add Ninject as main IoC Container + Automapper + SimpleCrypto

---
+++ ExampleDBEntities/Auction.Web/App_Start/AutoMapperConfig.cs
@@ -0,0 +1,12 @@
+﻿namespace Auction.Web
+{
+    public static class AutoMapperConfig
+    {
+        public static void RegisterMappings()
+        {
+            //AutoMapper.Mapper.CreateMap<Book, BookViewModel>()
+            //    .ForMember(dest => dest.Author,
+            //               opts => opts.MapFrom(src => src.Author.Name));
+        }
+    }
+}
---
+++ ExampleDBEntities/Auction.Web/App_Start/DependencyResolver.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using Auction.Data.Repository;
+using Auction.Data.UnitOfWork;
+using Ninject;
+
+
+namespace Auction.Web
+{
+    public class AuctionDependencyResolver : IDependencyResolver
+    {
+        private readonly IKernel _kernel;
+
+        public AuctionDependencyResolver(IKernel kernel)
+        {
+            _kernel = kernel;
+            DependencyBuilder();
+        }
+
+        private void DependencyBuilder()
+        {
+            _kernel.Bind<IUnitOfWork>().To<UnitOfWork>();
+            _kernel.Bind<IUserRepository>().To<UserRepository>();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return _kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return _kernel.GetAll(serviceType);
+        }
+    }
+}
