1c22186d1de3395a591d545bed128da808346cd4
Sanya Kaser <test.test@gmail.com>
2015-12-06 20:30:44 +0200

    Register AuctionRealtor area + add basic css + added WebConfigHelper

---
+++ ExampleDBEntities/Auction.Web/Areas/AuctionRealtor/AuctionRealtorAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Auction.Web.Areas.AuctionRealtor
+{
+    public class AuctionRealtorAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "AuctionRealtor";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "AuctionRealtor_default",
+                "AuctionRealtor/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ ExampleDBEntities/Auction.Web/Helpers/WebConfigHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.Configuration;
+
+namespace Auction.Web.Helpers
+{
+    public class WebConfigHelper
+    {
+        public static string ApplicationName {
+            get { return GetByKey("appName"); }
+        }
+
+        public static string GetByKey(string key)
+        {
+            return ConfigurationManager.AppSettings[key];
+        }
+    }
+}
