d3f2fe3f5e59421852b507731ecc30a7179d6a10
Sanya Kaser <test.test@gmail.com>
2015-12-06 21:51:16 +0200

    Added crypto utils + crypto utils tests

---
+++ ExampleDBEntities/Auction.Web/Utils/CryproUtil.cs
@@ -0,0 +1,26 @@
+﻿using SimpleCrypto;
+
+namespace Auction.Web.Utils
+{
+    public class CryproUtil
+    {
+        public PBKDF2 Pbkdf2 { get; private set; }
+
+        public CryproUtil()
+        {
+            Pbkdf2 = new PBKDF2();
+        }
+
+        public string HashText(string plainText)
+        {
+            return Pbkdf2.Compute(plainText);
+        }
+
+        public bool EqualsTo(string plainText, string hashedText)
+        {
+            return Pbkdf2.Compute(plainText, Pbkdf2.Salt).Equals(hashedText);
+        }
+
+
+    }
+}
---
+++ ExampleDBEntities/Test.Auction.Web/Utils/CryptoUtilTest.cs
@@ -0,0 +1,29 @@
+﻿using Auction.Web.Utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test.Auction.Web.Utils
+{
+    [TestClass]
+    public class CryptoUtilTest
+    {
+        private const string Password = "123";
+        private readonly CryproUtil _cryptoUtil;
+
+        public CryptoUtilTest()
+        {
+            _cryptoUtil = new CryproUtil();
+        }
+
+        [TestMethod]
+        public void StringToSameHashedStringShouldReturnSuccess()
+        {
+            Assert.IsTrue(_cryptoUtil.EqualsTo(Password, _cryptoUtil.HashText(Password)));
+        }
+
+        [TestMethod]
+        public void StringToAcotherHashedStringShouldReturnError()
+        {
+            Assert.IsFalse(_cryptoUtil.EqualsTo(Password, _cryptoUtil.HashText("456")));
+        }
+    }
+}
